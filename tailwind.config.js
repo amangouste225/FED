134078ddce0f9715e0754ebafcdc0a7132dfcbcb
Amangoua Ange <adagou@yahoo.com>
2024-01-31 12:17:16 +0000

    project 3 completed

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontSize: {
+        heading1: "14rem",
+      },
+      lineHeight: {
+        n: 0.8,
+      },
+      fontFamily: {
+        Gro: "Grotesk",
+      },
+    },
   },
   plugins: [],
 };
